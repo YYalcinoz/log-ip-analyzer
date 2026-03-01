0c1a97d80974bf2e2164e7c65d4e66aaec850acd
Yağız Yalçınöz <fyyalcinoz@gmail.com>
2026-03-01 16:13:40 +0300

    Add initial script and test log

---
+++ ip_analysis.py
@@ -0,0 +1,16 @@
+import re
+from collections import Counter
+
+with open("log.txt", "r", encoding="utf-8") as file:
+    data = file.read()
+
+
+ips = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", data)
+
+
+ip_count = Counter(ips)
+
+
+print("Bulunan IP adresleri:\n")
+for ip, count in ip_count.items():
+    print(f"{ip} -> {count} kez")
