334f78d9dca8f9b4127baa77825ec49439f347dc
Yağız Yalçınöz <fyyalcinoz@gmail.com>
2026-03-01 16:38:15 +0300

    Script update

--- ip_analysis.py
+++ ip_analysis.py
@@ -4,12 +4,9 @@ from collections import Counter
 with open("log.txt", "r", encoding="utf-8") as file:
     data = file.read()
 
-
 ips = re.findall(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b", data)
 
-
 ip_count = Counter(ips)
-
 
 print("Bulunan IP adresleri:\n")
 for ip, count in ip_count.items():
